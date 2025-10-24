4dee2e0961698520214df26799944e0425a03b5f
Konrad <kszczepanowski55@gmail.com>
2025-10-24 13:09:40 +0200

    Refactor Header tests to remove unused fireEvent import

--- src/components/tests/Header.test.tsx
+++ src/components/tests/Header.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
 describe('Header', () => {
